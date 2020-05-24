9dc00a963f1aa05f7bfc2a92fdd86d888214cd18
Avinash Dhawan <avinash.dhawan@gmail.com>
2020-05-23 23:15:43 -0400

    final version

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -28,31 +28,40 @@ form.on("submit",dataFilter);
 
 function dataFilter() {
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
+  var inputElement1 = d3.select("#datetime");
+  var inputElement2 = d3.select("#city");
+  var inputElement3 = d3.select("#state");
+  var inputElement4 = d3.select("#country");
+  var inputElement5 = d3.select("#shape");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputValue = inputElement1.property("value") ||inputElement2.property("value") ||inputElement3.property("value") ||inputElement4.property("value") ||inputElement5.property("value");
   if(inputValue){
     console.log(inputValue);
   console.log(tableData);
-  
-// filter data and store in filtered data
-  var filteredData = tableData.filter(tableDatum => tableDatum.datetime === inputValue);
+
+  // var filteredData = tableData.filter(tableDatum => tableDatum.datetime === inputValue);
+  var filteredData = tableData.filter(tableDatum => tableDatum.datetime === inputValue ||  tableDatum.city === inputValue ||  tableDatum.state === inputValue ||  tableDatum.country === inputValue ||  tableDatum.shape === inputValue);
 
   console.log(filteredData)
+
+  //Clear filters - show all data
   tbody.html("")
+
+  //Add each row of filtered data to HTML
   filteredData.forEach(function(UFOFiltereddata) {
     console.log(UFOFiltereddata);
     var row = tbody.append("tr");
     Object.entries(UFOFiltereddata).forEach(function([fkey, fvalue]) {
       console.log(fkey, fvalue);
 
+      // Append a cell to the row for each value
+      // in the UFO data object
       var fcell = row.append("td");
       fcell.text(fvalue);
-});
+    });
 
-});
+  });
 
 
   }
